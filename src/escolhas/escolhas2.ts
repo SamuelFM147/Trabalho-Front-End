97bfa92d82917f92e48c6464d1ede6451b544413
Baruch <153551584+BaruchManiac@users.noreply.github.com>
2025-06-09 20:06:23 -0300

    escolhas jogo 2 modelo json

---
+++ src/escolhas/escolhas2.ts
@@ -0,0 +1,18 @@
+// {
+//     "id": "001",
+//     "pergunta": "Você é o novo Vigia?",
+//     "imagem": "vigia_duvida.png",
+//     "escolhas": {
+//       "SIM": {
+//         "consequencia": "Bravo. Um mártir a caminho do esquecimento.",
+//         "salvos": 5,
+//         "sacrificados": 1
+//       },
+//       "NAO": {
+//         "consequencia": "Sua fuga foi poética. Quase.",
+//         "salvos": 0,
+//         "sacrificados": 3
+//       }
+//     }
+//   }
+  
